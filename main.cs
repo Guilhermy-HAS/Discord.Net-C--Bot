4d5aa738cc6e4ddd67fbfd2f7f29463488eee1e1
Dogao gostoso <77300993+Guilhermy-HAS@users.noreply.github.com>
2021-05-23 00:49:42 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
-using Discord.Websocket
+using Discord.Websocket;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TutorialBot
